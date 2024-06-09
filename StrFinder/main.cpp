d683d570a7405ee3f1b47738407ff45ed3cc9f84
Илья Маковский <makoi199563@gmail.com>
2024-06-10 00:04:20 +0300

    ver 2.0

--- StrFinder/main.cpp
+++ StrFinder/main.cpp
@@ -1,37 +1,53 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <cmath>
 
 using namespace std;
 
-// простая хеш-функция (для примера)
-int hash_function(const string& s) {
+//  простая хеш-функция (для примера) *с учетом предыдущего хеша*
+int hash_function(const string& s, int prime = 101) {
     int hash = 0;
     for (char c : s) {
-        hash += int(c);
+        hash = (hash * prime + int(c));
+        //cout << hash << endl;
     }
+    //cout << endl;
     return hash;
 }
 
-// алгоритм Рабина-Карпа
+int recalculate_hash(const string& s, int old_index, int new_index, int old_hash, int pattern_length, int prime = 101) {
+    //cout << old_hash << " " << int(s[old_index]) * pow(prime, pattern_length - 1) << " ";
+    int new_hash = old_hash - int(s[old_index]) * pow(prime, pattern_length - 1);
+    //cout << new_hash << " ";
+    new_hash = new_hash * prime + int(s[new_index]);
+    //cout << new_hash << endl << endl;
+    return new_hash;
+}
+
+// Алгоритм Рабина-Карпа
 int rabin_karp(const string& text, const string& pattern) {
     int n = text.size();
     int m = pattern.size();
-    int pattern_hash = hash_function(pattern);
+    int prime = 101; // простое число для хеш-функции
+
+    int pattern_hash = hash_function(pattern, prime);
+    int text_hash = hash_function(text.substr(0, m), prime);
+    //cout << "old_hash  -  new_hash   new_hash+" << endl;
 
     for (int i = 0; i <= n - m; ++i) {
-        int sub_hash = hash_function(text.substr(i, m));
-        if (sub_hash == pattern_hash) {
+        if (pattern_hash == text_hash) {
             if (text.substr(i, m) == pattern) {
-                return i; // Подстрока найдена на позиции i
+                return i;
             }
+        }
+
+        if (i < n - m) {
+            text_hash = recalculate_hash(text, i, i + m, text_hash, m, prime);
         }
     }
 
-    return -1; // Подстрока не найдена
+    return -1;
 }
-
-
 
 int main() {
     string text = "abracadabra";
